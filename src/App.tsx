91a2077a4e2228d9f73242e6938e5f8e3b30bd8e
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-06-13 16:40:40 -0300

    :construction: create global styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styled/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <GlobalStyle />
     </div>
   );
 }
