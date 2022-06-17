aa01f1723caecb3053652c1a199e56b2030727eb
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-06-17 15:42:41 -0300

    : construction: listando transacoes e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createServer, Model } from "miragejs"
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
   models: {
     transaction: Model,
   },
+seeds(server) {
+  server.db.loadData({
+    transactions: [
+      {
+        id: 1,
+        title: 'Freelance de website',
+        type: 'deposit',
+        category: 'Dev',
+        amount: 6000,
+        createdAt: new Date('2022-02-12 09:00:00'),
+      },
+      {
+        id: 2,
+        title: 'Conta de Luz',
+        type: 'withdraw',
+        category: 'Casa',
+        amount: 200,
+        createdAt: new Date('2022-04-12 09:00:00'),
+      },
+      
+    ],
+  })
+},
 
-  routes() {
-    this.namespace = 'api';
+routes() {
+    this.namespace = "api";
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions');
-    })
+      return this.schema.all('transaction');
+    });
 
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
 
-      return schema.create('transactions', data)
-    })
-  }
-})
+      return schema.create('transactions', data);
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
