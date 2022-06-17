08b8a6223a5661e2a6b704e1ed7ebaee8aec3b11
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-06-16 23:05:13 -0300

    :construction: Configurando model de criaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styled/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      {/* Funcão componente para alterar a o handle filho */}
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transactions</h2>
+        </Modal>
       <GlobalStyle />
     </ >
   );
