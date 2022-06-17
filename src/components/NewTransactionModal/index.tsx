e0ba088b43f53d0c3bdaea57e1d8ced9bd59bbf4
fernanda dias silva <f.dias.da.silva@gmail.com>
2022-06-17 15:03:29 -0300

    :construction: inserindo transacoes na api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 export interface NewTransactionModalProps {
@@ -22,6 +23,16 @@ export function NewTransactionModal({
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
+
+    const data = {
+      title, 
+      value, 
+      category,
+      type
+    };
+  
+    api.post('/transactions', data)
+
   }
 
   return (
